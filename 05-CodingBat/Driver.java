2c3a4ab1f316d9771bbe37f06abf0f3aeebbe44e
RyanTu <tu8.rya@gmail.com>
2014-09-25 01:16:57 -0400

    Added lab

--- 05-CodingBat/Driver.java
+++ 05-CodingBat/Driver.java
@@ -12,6 +12,26 @@ public class Driver{
 	System.out.println(a.diff21(19)); //returns 2
 	// diff21 test: n > 21
 	System.out.println(a.diff21(50)); //returns 58
+
+	// nearHundred tests
+	System.out.println(a.nearHundred(90)); //returns true
+	System.out.println(a.nearHundred(89)); //returns false
+	System.out.println(a.nearHundred(210)); //returns true
+	System.out.println(a.nearHundred(211)); //return false
+
+	// mixStart tests
+	System.out.println(a.mixStart("mix snacks")); //returns true
+	System.out.println(a.mixStart("piz snacks")); //returns false
+	System.out.println(a.mixStart("ni")); //returns false
+
+	// teaParty tests
+	System.out.println(a.teaParty(3,8)); //returns 0
+	System.out.println(a.teaParty(5,10)); //returns 2
+	System.out.println(a.teaParty(11,6)); //returns 1
+
+	// lastDigit tests
+	System.out.println(a.lastDigit(23,19,13)); //returns true
+	System.out.println(a.lastDigit(23,19,12)); //returns false
     }
 }
 
--- 05-CodingBat/StringStuff.java
+++ 05-CodingBat/StringStuff.java
@@ -20,4 +20,35 @@ public class StringStuff{
 	}   
     }
 
+    /*working*/
+    public boolean nearHundred(int n) {
+	return (Math.abs(100 - n) <= 10 || Math.abs(200-n) <= 10);
+    }
+
+    /*working*/
+    public boolean mixStart(String str) {
+	if (str.length() < 3){
+	    return false;
+	}
+	String check = str.substring(1,3);
+	return check.equals("ix");
+    }
+
+    /*working*/
+    public int teaParty(int tea, int candy) {
+	if (tea < 5 || candy < 5){
+	    return 0;
+	} else if (tea >= candy * 2 || candy >= tea * 2){
+	    return 2;
+	} else
+	    return 1; 
+    }
+
+    /*working*/
+    public boolean lastDigit(int a, int b, int c) {
+	a = a % 10;
+	b = b % 10;
+	c = c % 10;
+	return (a == b) || (a == c) || (b == c);
+    }
 }
