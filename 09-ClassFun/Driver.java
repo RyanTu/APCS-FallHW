4ae781712fb848ab26b9e26b7e40294044270b82
RyanTu <tu8.rya@gmail.com>
2014-10-09 05:04:53 -0400

    Homework 10

--- 09-ClassFun/Driver.java
+++ 09-ClassFun/Driver.java
@@ -3,34 +3,35 @@ public class Driver {
 	Basechar c = new Basechar();
 	Warrior w = new Warrior();
 	Mage m = new Mage();
+	Basechar c2;
+	Warrior w2;
+	Mage m2;
 
-	System.out.println(c.getHealth());
-	System.out.println(w.getHealth());
-	System.out.println(m.getHealth());
+	// nope w2 = m;
+	// nope w2 = c;
 
-	System.out.println(m.getManna());
+	// this works -- note that c2.getHealth() calls getHealth in the
+	// warrior subclass
+	c2 = w;
+        System.out.println(c2.getHealth());
 
-	Basechar b1,b2;
-	Warrior w1,w2;
-	Mage m1,m2;
+	c2 = m;
+        System.out.println(c2.getHealth());
+        System.out.println( ((Mage)c2).getManna()   );
 
-	b1 = new Basechar();
-	w1 = new Warrior();
-	m1 = new Mage();
+	// w.setName("Groo");
+	// m.setName("Mr. Sage");
 
-	m2 = m1;
-	/* m2 is a variable that directs to Mage@___ (same as m1)*/
-	// m2 = b1;
-	// incompatible types required: Mage found: Basechar
-	// m2 = c1;
-	// cannot find c
+	w.attack(m);
+	m.attack(w);
+	w.attack(w);
+	System.out.println(w.toString());
+	
+	System.out.println(w);
 
-	b2 = m1;
-	/* b2 directs to Mage@___ (same as m1), no access to getManna */
-	b2 = m2;
-	/* b2 directs to Mage@___ (same as m2), no access to getManna */
-	b2 = new Warrior();
-	/* b2 directs to Warrior@___, no access to healthboost */
+
+	m2 = new Mage("Billy",50);
+
+	System.out.println(m2+ " has manna "+ ((Mage)m2).getManna());
     }
-
 }
--- 09-ClassFun/Mage.java
+++ 09-ClassFun/Mage.java
@@ -1,8 +1,26 @@
 public class Mage extends Basechar{
     private int manna =12345;
+    private String name;
+
+    public Mage(String s){
+	name = s;
+    }
+
+    public Mage(){
+	name = "Generic";
+    }
+
+    public Mage(String n, int m){
+	name = n;
+	manna = m;
+    }
 
     public int getManna(){
 	return manna;
+    }
+
+    public String getName(){
+	return name;
     }
     
 }
