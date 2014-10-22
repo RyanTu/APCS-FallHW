0078a31536857e6092920b50b33826fb92a5b189
RyanTu <tu8.rya@gmail.com>
2014-10-22 04:17:23 -0400

    Stuyablo

---
+++ 12-Stuyablo/BaseChar.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class BaseChar {
+    private int Health = 100;
+    private int Mana = 10;
+    private int Attack = 20;
+    private int Dexterity = 20;
+    
+    public int getHealth(){
+	return Health;
+    }
+    
+    public int getMana(){
+	return Mana;
+    }
+    public int getAttack(){
+	return Attack;
+    }
+    public int getDexterity(){
+	return Dexterity;
+    }
+
+}
---
+++ 12-Stuyablo/Warrior.java
@@ -0,0 +1,21 @@
+public class Warrior extends BaseChar {
+    private int healthboost = 5;
+    private int strengthboost = 5;
+    
+    
+    public int getHealth() {
+	return (healthboost + super.getHealth());
+    }
+    public int getMana() {
+	return super.getMana();
+    }
+    public int getAttack() {
+	return (strengthboost + super.getAttack());
+    }
+    public int getDexterity() {
+	return super.getDexterity();
+    }
+    public void attack(BaseChar other){
+	System.out.println(this+" is SUPERattacking "+other);
+    }
+}
