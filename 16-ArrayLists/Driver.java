f9a59034616f6e1be633e14187bb6664c3b9d192
RyanTu <tu8.rya@gmail.com>
2014-11-12 22:34:34 -0500

    HW 16

--- 16-ArrayLists/Driver.java
+++ 16-ArrayLists/Driver.java
@@ -9,5 +9,20 @@ public class Driver{
 	for (int i = 0; i<20; i++){
 	    ai.add(r.nextInt(5));
 	}
+
+	// System.out.println(ai);
+
+	int temp = ai.get(0);
+	int i = 1;
+	while (i<ai.size()){
+	    if (temp == ai.get(i)){
+		temp = ai.remove(i);
+	    } else {
+		temp = ai.get(i);
+		i++;
+	    }
+	}
+
+	// System.out.println(ai);
     }
 }
