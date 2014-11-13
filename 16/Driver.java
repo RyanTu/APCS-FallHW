c756188ec977cf03035a8af194ffbc3d009abecd
ryan.tu <tu8.rya@gmail.com>
2014-11-12 21:25:40 -0500

    Class

---
+++ 16/Driver.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Driver{
+    public static void main(String[] args){
+
+	ArrayList<Integer> ai = new ArrayList<Integer>();
+
+	Random r = new Random();
+	for (int i = 0; i<20; i++){
+	    ai.add(r.nextInt(5));
+	}
+
+	int temp;
+	for (int i = 0; i<ai.length-1; i++){
+	    temp = ai[i];
+	    if (temp == ai[i+1]){
+		
+    }
+}
