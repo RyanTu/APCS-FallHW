d4d9ec2f63e45c537cf9f2453501d2ff98322b81
RyanTu <tu8.rya@gmail.com>
2014-11-15 09:56:32 -0500

    HW 17

---
+++ 17-ArrayLists/RandomA.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class RandomA{
+    int[] a;
+    public RandomA(int n){
+	a = new int[n];
+	for (int i = 0; i<a.length; i++){
+	    a[i] = i;
+	}
+    } 
+    public RandomA(){
+	this(10);
+    }
+
+    Random r = new Random();
+    public void rand(){
+	for (int i = 0; i<a.length; i++){
+	int r1 = r.nextInt(a.length);
+	int r2 = r.nextInt(a.length);
+	int store = a[r1];
+	a[r1] = a[r2];
+	a[r2] = store;
+	}
+	for (int i = 0; i<a.length; i++){
+	    System.out.print(a[i] + " ");
+	}
+    }
+}
---
+++ 17-ArrayLists/RandomAL.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class RandomAL{
+    ArrayList<Integer> al = new ArrayList<Integer>();
+    public RandomAL(int n){
+	for (int i = 0; i<n; i++){
+	    al.add(i);
+	}
+    }
+    public RandomAL(){
+	this(10);
+    }
+
+    Random r = new Random();
+
+    public ArrayList<Integer> rand(){
+	ArrayList<Integer> temp = new ArrayList<Integer>();
+	int index;
+	// System.out.println(al);
+	while (al.size()>0){
+	    index = r.nextInt(al.size());
+	    temp.add(al.get(index));
+	    al.remove(index);
+	}
+	al = temp;
+	return al;
+    }
+}
