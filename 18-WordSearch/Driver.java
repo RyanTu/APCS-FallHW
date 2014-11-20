93826f419e3ff755fafc27e1d57c87f1237e3d8a
RyanTu <tu8.rya@gmail.com>
2014-11-19 21:57:32 -0500

    HW 18 Part 1

--- 18-WordSearch/Driver.java
+++ 18-WordSearch/Driver.java
@@ -1,7 +1,8 @@
 public class Driver{
     public static void main(String[] args){
 	WordSearch w = new WordSearch();
-	w.addWord("hello");
+	w.addWord("hello" /* TEST ,1,1 */);
+	w.addWord("bye" /* TEST ,1,1 */);
 	System.out.println(w);
     }
 }
--- 18-WordSearch/WordSearch.java
+++ 18-WordSearch/WordSearch.java
@@ -33,26 +33,40 @@ public class WordSearch {
 	return s;
     }
 
-    public void addWord(String w){
+    public void addWord(String w /* TEST ,int x, int y */){
 	int l = w.length()-1;
 	boolean fits = false;
+	boolean overlap = false;
 	while(!fits){
 	    int y = r.nextInt(boardX);
 	    int x = r.nextInt(boardY);
 	    int xVel=0;
 	    int yVel=0;
+
+	    // controls direction the word is written in
 	    while(xVel==0 && yVel==0){
 		// xVel = r.nextInt(3) - 1;
 		yVel = r.nextInt(3) - 1;
 	    }
+
 	    if((x+l*xVel>=0)&&(x+l*xVel<boardX)&&
 	       (y+l*yVel>=0)&&(y+l*yVel<boardY)){
 		for (int i = 0; i<w.length(); i++){
-		    board[x][y] = w.charAt(i);
-		    y += 1*yVel;
-		    // y += 1*yVel;
+		    if (board[x][y] != '.'){
+			overlap = true;
+		    }
 		}
-		fits = true;
+
+		if (!overlap){
+		    for (int i = 0; i<w.length(); i++){
+			board[x][y] = w.charAt(i);
+			y += 1*yVel;
+			// y += 1*yVel;
+			if (i == w.length() - 1){
+			    fits = true;
+			}
+		    }
+		}
 	    }
 	}
     }
