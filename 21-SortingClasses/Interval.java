e23b7ead88b2db351a32e20f59f7d3ca4c6d1e70
RyanTu <tu8.rya@gmail.com>
2014-12-10 06:15:06 -0500

    HW 21

--- 21-SortingClasses/Interval.java
+++ 21-SortingClasses/Interval.java
@@ -6,7 +6,7 @@ public class Interval{
     private static Random r = new Random();
     private int low, high;
 
-    private void setup(int i, int h){
+    private void setup(int l, int h){
 	high = h;
 	low = l;
     }
@@ -26,15 +26,37 @@ public class Interval{
 	return s;
     }
 
+    public int getLow(){
+	return low;
+    }
+
+    public int getHigh(){
+	return high;
+    }
+
+    public int compareTo(Interval other){
+	if (this.getLow() != other.getLow()){
+	    return this.getLow()-other.getLow();
+	} else {
+	    return this.getHigh() - other.getHigh();
+	}
+    }
+
     public static void main(String[] args){
+	/*
 	for (int i = 0; i<10; i++){
 	    System.out.println(new Interval());
 	}
 
-	Interval[a] = new Interval[10];
+	Interval[] a = new Interval[10];
 	for (int i = 0; i<a.length; i++){
-	    a[i] new Interval();
+	    a[i] = new Interval();
+
 	}
 	System.out.println(Arrays.toString(a));
+	*/
+	Interval i1 = new Interval(9, 12);
+	Interval i2 = new Interval(9, 12);
+	System.out.println(i1.compareTo(i2));
     }
 }
